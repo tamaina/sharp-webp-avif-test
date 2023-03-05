06d3f1b79107ff46f225f04090ee0ab6c9bd159a
tamaina <tamaina@hotmail.co.jp>
2023-03-05 17:42:01 +0000

    :v:

--- entropies.js
+++ entropies.js
@@ -3,7 +3,6 @@ import sharp from "sharp";
 
 for (const file of globSync("*.{jpg,jpeg,png}")) {
     const ctx = sharp(file);
-    const metadata = await ctx.metadata();
     const stats = await ctx.stats();
-    console.log(file, metadata.format, metadata.width, metadata.height, stats.entropy);
+    console.log(stats.entropy.toFixed(4), file);
 }
