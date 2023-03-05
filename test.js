84bdbc8913a0244e77e905a9312342e6b08e9c80
tamaina <tamaina@hotmail.co.jp>
2023-03-05 17:23:49 +0000

    tsumugi

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
 import sharp from "sharp";
 import { statSync } from "node:fs";
 
-const source = "test.3.png";
+const source = "test.4.jpg";
 
 const ctx = sharp(source);
 const metadata = await ctx.metadata();
-console.log(metadata.format, metadata.width, metadata.height);
+const stats = await ctx.stats();
+console.log(source);
+console.log(metadata.format, metadata.width, metadata.height, stats.entropy);
 console.log((statSync(source).size / 1000).toFixed(0), "KB");
 console.log();
 
@@ -15,8 +17,8 @@ const webp = {
     lossless: false,
     nearLossless: false,
     alphaQuality: 100,
-    smartSubsample: false,
-    effort: 0,
+    smartSubsample: true,
+    effort: 1,
 };
 console.log("WEBP");
 console.log(webp);
@@ -31,9 +33,9 @@ console.log();
 
 timer = process.hrtime();
 const avif = {
-    quality: 50,
+    quality: 60,
     lossless: false,
-    effort: 0,
+    effort: 1,
 };
 console.log("AVIF");
 console.log(avif);
